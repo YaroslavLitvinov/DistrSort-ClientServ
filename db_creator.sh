ef2f75afe86f96c12bfa64fa21abd0d4acae0927
YaroslavLitvinov <yaroslav.litvinov@gmail.com>
2012-05-20 20:16:22 +0300

    build-run commands added, to README

--- db_creator.sh
+++ db_creator.sh
@@ -1,5 +1,5 @@
 rm server_dsort.db client_dsort.db -f
-~/Загрузки/Development/sqlite-autoconf-3071100/sqlite3 server_dsort.db < sql/db_server_dsort.sql
-~/Загрузки/Development/sqlite-autoconf-3071100/sqlite3 client_dsort.db < sql/db_client_dsort.sql
+~/sqlite-autoconf-3071200/sqlite3 server_dsort.db < sql/db_server_dsort.sql
+~/sqlite-autoconf-3071200/sqlite3 client_dsort.db < sql/db_client_dsort.sql
 ls -l *.db
 echo "Databases created."

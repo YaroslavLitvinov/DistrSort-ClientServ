2338c583de589b79344293cfcd471fd23bf49c83
YaroslavLitvinov <yaroslav.litvinov@gmail.com>
2012-05-30 12:05:36 +0300

    added support for 50 src nodes. added sql configuration files for db for 50nodes; changed node ids in appropriated files: defines.h and shell scripts

--- db_creator.sh
+++ db_creator.sh
@@ -1,5 +1,5 @@
 rm server_dsort.db client_dsort.db -f
-/usr/local/bin/sqlite3 server_dsort.db < sql/db_server_dsort.sql
-/usr/local/bin/sqlite3 client_dsort.db < sql/db_client_dsort.sql
+/usr/local/bin/sqlite3 server_dsort.db < sql/db_server_dsort-50.sql
+/usr/local/bin/sqlite3 client_dsort.db < sql/db_client_dsort-50.sql
 ls -l *.db
 echo "Databases created."
